d99b95960307b7500e344dfe6b9576541d0cecd5
Shawn Stern <stern.shawn@gmail.com>
2017-07-10 13:37:07 -0700

    Adding contextTypes to Authentication HOC to expose react-router methods. Adding comments to help internalize learning re. static keyword, context, etc

--- udemy/React-Redux/higher-order-components-0/src/components/require_auth.js
+++ udemy/React-Redux/higher-order-components-0/src/components/require_auth.js
@@ -3,6 +3,14 @@ import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+    // Define the contextTypes property on the Authentication class. 'static' keyword lets us define
+    // it here instead of doing Authentication.contextTypes ={...} outside of the declaration
+    // This exposes context that is otherwise hidden by React unless explicitly defined to avoid abuse
+    static contextTypes = {
+      router: React.PropTypes.object,
+    }
+
+    // Render the passed component, and pass down any addition properties that we've added using ES6 spread
     render() {
       return <ComposedComponent {...this.props} />
     }

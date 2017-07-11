634c0380ab6ee02dbece58df198bfbc5ebad4b93
Shawn Stern <stern.shawn@gmail.com>
2017-07-11 15:58:59 -0700

    Wiring up Passport and our strategy as a middleware to prevent unauthroized access to a data endpoint

--- udemy/React-Redux/auth-server/router.js
+++ udemy/React-Redux/auth-server/router.js
@@ -1,5 +1,15 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+// Middleware to authenticate using jwt, and DON'T create a cookie session (since we're doing jwt anyway)
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = (app) => {
+  // DUmmy route to test if you get unauthroized if making a GET without a valid authorization header
+  app.get('/', requireAuth, (req, res) => {
+    res.send({ hi: 'You are authenticated!' });
+  });
+
   app.post('/signup', Authentication.signup);
 }

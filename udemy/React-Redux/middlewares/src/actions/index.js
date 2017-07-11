97531a5f44bd1b549f2266b130facf538836abd2
Shawn Stern <stern.shawn@gmail.com>
2017-07-10 22:34:10 -0700

    Adding custom middleware to handle async retrieval of users on action dispatch
    -Bringing in the axios library to create an ajax promise from an API. When the page is loaded, it dispatches an action of type FETCH_USERS with a payload being the request promise
    -We've added a middleware which looks at all actions, and if the action is a promise, waits for the promise to resolve and then dispatches a copy of the incoming action, but with the payload replaced with the response value! If the incoming action is not a promise, or it has no payload, it is simply passed along with no interaction.
    -Updating the reducer to make use of the action.payload.data field instead of just action.payload, since the list of users will be in the response's data field...

--- udemy/React-Redux/middlewares/src/actions/index.js
+++ udemy/React-Redux/middlewares/src/actions/index.js
@@ -1,12 +1,11 @@
+import axios from 'axios';
 import { FETCH_USERS } from './types';
 
 export function fetchUsers() {
+  const request = axios.get('https://jsonplaceholder.typicode.com/users');
+
   return {
     type: FETCH_USERS,
-    payload: [
-      { name: 'Brady' },
-      { name: 'Shawn' },
-      { name: 'Stevie' },
-    ],
+    payload: request,
   };
 }
--- udemy/React-Redux/middlewares/src/middlewares/async.js
+++ udemy/React-Redux/middlewares/src/middlewares/async.js
@@ -1,10 +1,24 @@
+import axios from 'axios';
+
 export default function({ dispatch }) {
   return next => action => {
-    // Log the intercepted action
-    console.log(action);
+    // If the action is not a promise, don't interact with it, just pass it along
+    if (!action.payload || !action.payload.then) {
+      return next(action);
+    }
 
-    // Pass it on to the next middleware in the chain (if any)
-    // or to the reducers if this is the last middleware
-    next(action);
+    // This action has a promise, wait for it to resolve, then dispatch a new action
+    // with the resolved Promise's data as the payload
+    action.payload
+      .then(response => {
+        // Create a new action with the existing action's type, etc, but with the promise's response
+        // as the new payload value
+        const newAction = {
+          ...action,
+          payload: response
+        };
+
+        dispatch(newAction);
+      });
   }
 }

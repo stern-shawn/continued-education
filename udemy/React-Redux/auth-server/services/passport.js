c39aa391293ae738dab4a4f194b8b2d99c92db92
Shawn Stern <stern.shawn@gmail.com>
2017-07-11 15:43:04 -0700

    Adding JWT and Passport libraries. Creating a JwtStrategy which checks if the user inside the given token exists to determine if the request should be authenticated or not

---
+++ udemy/React-Redux/auth-server/services/passport.js
@@ -0,0 +1,29 @@
+const passport = require('passport');
+const User = require('../models/User');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// Set up the options for the JWT Strategy
+const jwtOptions = {
+
+};
+
+// Create JWT Strategy
+const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
+  // See if the user ID in this payload exists in our DB, user is valid, call done with that user
+  // Otherwise, just call done to indicate no auth success
+  User.findById(payload.sub)
+    .then(user => {
+      // Search found a matching user
+      if (user) {
+        done(null, user);
+      } else {
+        // Search succeeded but didn't return a user
+        done(null, false);
+      }
+    })
+    // Search failed
+    .catch(err => done(err, false));
+});
+
+// Tell Passport to use the strategy

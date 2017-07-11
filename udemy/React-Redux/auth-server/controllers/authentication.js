1d30a8aaecb86003fe4333459cc5257737ff310e
Shawn Stern <stern.shawn@gmail.com>
2017-07-11 12:14:03 -0700

    Adding Promises to mongoose and making a simple signup flow that errors if user tries to sign up with an email that's already in use

--- udemy/React-Redux/auth-server/controllers/authentication.js
+++ udemy/React-Redux/auth-server/controllers/authentication.js
@@ -1,3 +1,24 @@
+const User = require('../models/User');
+
 exports.signup = (req, res, next) => {
-  res.send({ success: 'true' });
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email })
+    .then((existingUser) => {
+      if (existingUser) {
+        return res.status(422).send({ error: 'Email is in use' });
+      }
+
+      // If a new email, create and save new user
+      const user = new User({
+        email,
+        password,
+      });
+
+      // Respond to the request indicating that new user has been created
+      user.save()
+        .then((user) => res.json(user))
+        .catch(err => next(err));
+    })
+    .catch(err => next(err));
 }

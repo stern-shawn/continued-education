cf847f322d7e343cbb3a78b9ddffbad8a16875f4
Shawn Stern <stern.shawn@gmail.com>
2017-07-10 21:55:03 -0700

    Adding a UserList component which dispatches the dummy fetch action for user data and renders a contact card for each user

---
+++ udemy/React-Redux/middlewares/src/components/userList.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class UserList extends Component {
+  componentWillMount() {
+    this.props.fetchUsers();
+  }
+
+  renderUser = (user) => {
+    return (
+      <div className="card card-block">
+        <h4 className="card-title">{user.name}</h4>
+        <p className="card-text">The Company</p>
+        <a className="btn btn-primary">Email</a>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.users.map(this.renderUser)}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  users: state.users,
+});
+
+export default connect(mapStateToProps, actions)(UserList);

88ccba27a4e6524fd33b0fada2eecd9ad0194a7a
Shawn Stern <stern.shawn@gmail.com>
2017-07-10 22:45:11 -0700

    Minor update to make use of the response data to display users' company and a link to their website

--- udemy/React-Redux/middlewares/src/components/userList.js
+++ udemy/React-Redux/middlewares/src/components/userList.js
@@ -11,8 +11,8 @@ class UserList extends Component {
     return (
       <div className="card card-block">
         <h4 className="card-title">{user.name}</h4>
-        <p className="card-text">The Company</p>
-        <a className="btn btn-primary">Email</a>
+        <p className="card-text">{user.company.name}</p>
+        <a className="btn btn-primary" href={`http://${user.website}`} target="_blank">Website</a>
       </div>
     );
   }

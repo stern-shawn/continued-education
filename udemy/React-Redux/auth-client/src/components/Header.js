f568c278fe88ee9d56808f4d18c178b0d987b1af
Shawn Stern <stern.shawn@gmail.com>
2017-07-12 15:14:02 -0700

    Connecting Header to state, and rendering sign in/up/out buttons depending on if user is authenticated or not

--- udemy/React-Redux/auth-client/src/components/Header.js
+++ udemy/React-Redux/auth-client/src/components/Header.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  // Conditionally render the sign in/up links if user is not authenticated,
+  // or the sign out link if they are authenticated
+  renderLinks = () => {
+    if (this.props.authenticated) {
+      return (
+        <li className="nav-item">
+          <Link className="nav-link" to="/signout">Sign Out</Link>
+        </li>
+      );
+    }
+
+    return [
+      <li className="nav-item" key="signin">
+        <Link className="nav-link" to="/signin">Sign In</Link>
+      </li>,
+      <li className="nav-item" key="signup">
+        <Link className="nav-link" to="/signup">Sign Up</Link>
+      </li>,
+    ];
+  }
+
   render() {
     return (
       <nav className="navbar navbar-light">
+        <Link to="/" className="navbar-brand">Redux Auth</Link>
         <ul className="nav navbar-nav">
-          <li className="nav-item">
-            Sign In
-          </li>
+          {this.renderLinks()}
         </ul>
       </nav>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  authenticated: state.auth.authenticated,
+});
+
+export default connect(mapStateToProps)(Header);

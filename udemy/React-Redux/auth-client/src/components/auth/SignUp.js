6d751430b7fade9a3eb3f82b63ac0bc638c9b4e3
Shawn Stern <stern.shawn@gmail.com>
2017-07-12 16:08:18 -0700

    Adding basic, unoptimized form validation to SignUp using redux form validation helper

--- udemy/React-Redux/auth-client/src/components/auth/SignUp.js
+++ udemy/React-Redux/auth-client/src/components/auth/SignUp.js
@@ -14,14 +14,17 @@ class SignUp extends Component {
         <fieldset className="form-group">
           <label>Email:</label>
           <input {...email} className="form-control" />
+          {email.touched && email.error && <div className="error">{email.error}</div>}
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
           <input {...password} type="password" className="form-control" />
+          {password.touched && password.error && <div className="error">{password.error}</div>}
         </fieldset>
         <fieldset className="form-group">
           <label>Confirm Password:</label>
           <input {...passwordConfirm} type="password" className="form-control" />
+          {passwordConfirm.touched && passwordConfirm.error && <div className="error">{passwordConfirm.error}</div>}
         </fieldset>
 
         <button action="submit" className="btn btn-primary">Sign Up</button>
@@ -30,7 +33,30 @@ class SignUp extends Component {
   }
 }
 
+const validate = (formProps) => {
+  const errors = {};
+
+  if (!formProps.email) {
+    errors.email = "Please enter an email";
+  }
+
+  if (!formProps.password) {
+    errors.password = "Please enter a password";
+  }
+
+  if (!formProps.passwordConfirm) {
+    errors.passwordConfirm = "Please enter a password confirmation";
+  }
+
+  if (formProps.password !== formProps.passwordConfirm) {
+    errors.password = 'Passwords must match';
+  }
+
+  return errors;
+};
+
 export default reduxForm({
   form: 'signup',
   fields: ['email', 'password', 'passwordConfirm'],
+  validate,
 }, null, actions)(SignUp);

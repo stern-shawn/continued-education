b314e9b8214ec0ab1ec46516f314008b611e70ae
Shawn Stern <stern.shawn@gmail.com>
2017-07-13 15:00:09 -0700

    Adding basic welcome page as the root IndexRoute

---
+++ udemy/React-Redux/auth-client/src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <div>
+      Welcome to our little slice of paradise. Please sign in or sign up to view this site's protected content!
+    </div>
+  );
+};
+
+export default Welcome;
--- udemy/React-Redux/auth-client/src/index.js
+++ udemy/React-Redux/auth-client/src/index.js
@@ -10,6 +10,7 @@ import Feature from './components/feature';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 import SignOut from './components/auth/SignOut';
+import Welcome from './components/Welcome';
 
 import RequireAuth from './components/auth/requireAuth';
 import reducers from './reducers';
@@ -20,6 +21,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
+        <IndexRoute component={Welcome} />
         <Route path="feature" component={RequireAuth(Feature)} />
         <Route path="signin" component={SignIn} />
         <Route path="signup" component={SignUp} />

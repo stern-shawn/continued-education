3f29c0c21d97e67ebc28d8ee86bec5e704b576df
Shawn Stern <stern.shawn@gmail.com>
2017-07-13 15:09:35 -0700

    Now this is cool. Modifying the store so that we check for the existence of a token in localStorage and dispatch the AUTH_USER action to the store if one is found BEFORE rendering the application. This way the user is still considered authenticated if they refresh, close and revisit the app, etc

--- udemy/React-Redux/auth-client/src/index.js
+++ udemy/React-Redux/auth-client/src/index.js
@@ -14,11 +14,21 @@ import Welcome from './components/Welcome';
 
 import RequireAuth from './components/auth/requireAuth';
 import reducers from './reducers';
+import { AUTH_USER } from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+// If the user has a token, consider them to already be signed in
+if (token) {
+  // Update state prior to render. Store has the dispatch method built in so we can dispatch actions
+  // at this level of the app!
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={Welcome} />

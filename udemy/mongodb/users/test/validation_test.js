15a0e932517c63e1607e2f40f5a4b6f3662ddb21
Shawn Stern <stern.shawn@gmail.com>
2017-07-23 09:13:37 -0700

    Adding a test which attempts to save invalid data, asserts that our validators catch the error

--- udemy/mongodb/users/test/validation_test.js
+++ udemy/mongodb/users/test/validation_test.js
@@ -2,6 +2,8 @@ const assert = require('assert');
 const User = require('../src/User.js');
 
 describe('Validating records', () => {
+  // The following two tests are checking the validation methods built into the User class,
+  // using the synchronous version to check for errors without actually saving to the DB
   it('requires a user name', () => {
     const user = new User({ name: undefined });
     const validationResult = user.validateSync(); // Synchronous version of validation op
@@ -16,5 +18,17 @@ describe('Validating records', () => {
     const { message } = validationResult.errors.name;
 
     assert(message === 'Name must be longer than 2 characters');
+  });
+
+  // This test attempts to save to the remote DB and checks if validators protect our db
+  it('prevents an invalid user from being saved', (done) => {
+    const user = new User({ name: 'Sh' });
+    user.save()
+      .catch((validationResult) => {
+        const { message } = validationResult.errors.name;
+
+        assert(message === 'Name must be longer than 2 characters');
+        done();
+      });
   });
 });

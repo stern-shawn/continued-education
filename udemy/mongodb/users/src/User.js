0a88852f83cccf0a4acf2b032b2aef845748624c
Shawn Stern <stern.shawn@gmail.com>
2017-07-23 09:07:11 -0700

    Adding a simple validator to check that user name is valid (>2 characters in length in this example)

--- udemy/mongodb/users/src/User.js
+++ udemy/mongodb/users/src/User.js
@@ -6,6 +6,10 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   name: {
     type: String,
+    validate: {
+      validator: name => name.length > 2,
+      message: 'Name must be longer than 2 characters',
+    },
     required: 'A name is required',
   },
   postCount: Number,
--- udemy/mongodb/users/test/validation_test.js
+++ udemy/mongodb/users/test/validation_test.js
@@ -9,4 +9,12 @@ describe('Validating records', () => {
 
     assert(message === 'A name is required');
   });
+
+  it('requires a user name to be longer than 2 characters', () => {
+    const user = new User({ name: 'Sh' });
+    const validationResult = user.validateSync(); // Synchronous version of validation op
+    const { message } = validationResult.errors.name;
+
+    assert(message === 'Name must be longer than 2 characters');
+  });
 });

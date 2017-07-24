1f4478ce8f3187fb1d400a74240644c3c4fdb8db
Shawn Stern <stern.shawn@gmail.com>
2017-07-24 10:39:02 -0700

    Adding a unit test to demonstrate that we can sort and pagniate users with skip + limit

--- udemy/mongodb/users/test/read_test.js
+++ udemy/mongodb/users/test/read_test.js
@@ -4,10 +4,17 @@ const User = require('../src/User.js');
 describe('Reading records', () => {
   // Declare joe ahead of time so we can have a reference to it in the 'it' blocks
   let joe;
+  let shawn;
+  let sean;
+  let adam;
 
   beforeEach((done) => {
     joe = new User({ name: 'Joe' });
-    joe.save()
+    shawn = new User({ name: 'Shawn' });
+    sean = new User({ name: 'Sean' });
+    adam = new User({ name: 'Adam' });
+
+    Promise.all([joe.save(), shawn.save(), sean.save(), adam.save()])
       .then(() => done());
   });
 
@@ -24,6 +31,22 @@ describe('Reading records', () => {
     User.findOne({ _id: joe._id })
       .then((user) => {
         assert(user.name === joe.name);
+        done();
+      });
+  });
+
+  it('can skip and limit the result set', (done) => {
+    // We don't know what order the records got saved in... so sort first so we can be sure
+    // of the results order. .sort expects a key and 1 for ascending, -1 for descending order.
+    User.find()
+      .sort({ name: 1 })
+      .skip(1)
+      .limit(2)
+      .then((users) => {
+        // Assert that limit worked correctly, and that results are correct based on skip
+        assert(users.length === 2);
+        assert(users[0].name === 'Joe');
+        assert(users[1].name === 'Sean');
         done();
       });
   });

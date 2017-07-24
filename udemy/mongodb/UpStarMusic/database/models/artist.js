608224f19ae74ea9cbedf6300eb9f55d847ba115
Shawn Stern <stern.shawn@gmail.com>
2017-07-24 11:17:16 -0700

    First pass at Artist Schema, will be using a subdocument for Albums instead of refs

--- udemy/mongodb/UpStarMusic/database/models/artist.js
+++ udemy/mongodb/UpStarMusic/database/models/artist.js
@@ -1,1 +1,19 @@
-// Todo: Create Artist Model
+const mongoose = require('mongoose');
+const AlbumSchema = require('./album');
+
+const Schema = mongoose.Schema;
+
+const ArtistSchema = new Schema({
+  name: String,
+  age: Number,
+  yearsActive: Number,
+  image: String,
+  genre: String,
+  website: String,
+  netWorth: Number,
+  labelName: String,
+  retired: Boolean,
+  albums: [AlbumSchema],
+});
+
+module.exports = mongoose.model('Artist', ArtistSchema);

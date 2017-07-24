c273405d3e7c455bfdac219559142f5bcded2712
Shawn Stern <stern.shawn@gmail.com>
2017-07-24 14:53:55 -0700

    Implementing the FindArtist query

--- udemy/mongodb/UpStarMusic/database/queries/FindArtist.js
+++ udemy/mongodb/UpStarMusic/database/queries/FindArtist.js
@@ -5,5 +5,4 @@ const Artist = require('../models/artist');
  * @param {string} _id - The ID of the record to find.
  * @return {promise} A promise that resolves with the Artist that matches the id
  */
-module.exports = (_id) => {
-};
+module.exports = (_id) => Artist.findById(_id);

8e79006a3fd83524f0e3b95171e2cc60522f81e9
Shawn Stern <stern.shawn@gmail.com>
2017-07-23 17:14:04 -0700

    Since we're now including Comments and BlogPosts in testing, also adding code to the test helper to drop these collections before tests

--- udemy/mongodb/users/test/test_helper.js
+++ udemy/mongodb/users/test/test_helper.js
@@ -18,7 +18,14 @@ before((done) => {
 // Clear the list of users before running a test to ensure we have a clean DB
 // Async action, so use the done() callback to control Mocha flow
 beforeEach((done) => {
-  mongoose.connection.collections.users.drop(() => {
-    done();
+  // BIG GOTCHA: mongoose normalizes collection names by fully lower-casing and pluralizing names
+  const { users, comments, blogposts } = mongoose.connection.collections;
+  // We cannot drop collections in parallel, so we have to do this sequentially...
+  users.drop(() => {
+    comments.drop(() => {
+      blogposts.drop(() => {
+        done();
+      });
+    });
   });
 });

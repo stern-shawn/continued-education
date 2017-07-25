3e2178151004b4151dc369fc6f10b0bd91935f95
Shawn Stern <stern.shawn@gmail.com>
2017-07-25 10:48:04 -0700

    Adding basic routing boilerplate and testing that the api responds as expected using supertest to mock requests

---
+++ udemy/mongodb/Muber/test/app_test.js
@@ -0,0 +1,14 @@
+// const assert = require('assert');
+// const request = require('supertest');
+// const app = ('../app');
+
+// describe('The express app', () => {
+//   it('handles a GET request to /api', (done) => {
+//     request(app)
+//       .get('/api')
+//       .end((err, res) => {
+//         assert(res.body.greeting === 'Hello');
+//         done();
+//       });
+//   });
+// });

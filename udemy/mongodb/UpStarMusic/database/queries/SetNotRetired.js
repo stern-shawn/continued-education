1ded5cec589cca004100450ed7f4b5740ccb0ef7
Shawn Stern <stern.shawn@gmail.com>
2017-07-25 09:09:46 -0700

    Learning how to update multiple records in one query when given an array of ids, using update with multi option set to true, or updateMany which is just a wrapper for update with the option set for you automatically

--- udemy/mongodb/UpStarMusic/database/queries/SetNotRetired.js
+++ udemy/mongodb/UpStarMusic/database/queries/SetNotRetired.js
@@ -5,5 +5,14 @@ const Artist = require('../models/artist');
  * @param {array} _ids - An array of the _id's of of artists to update
  * @return {promise} A promise that resolves after the update
  */
-module.exports = (_ids) => {
-};
+module.exports = (_ids) =>
+  // Trying out .updateMany instead of using .update with multi set to true
+  Artist.updateMany(
+    { _id: { $in: _ids } },
+    { $set: { retired: false } },
+  );
+
+/*
+According to mongoDB docs:
+	.updateMany - Equivalent to db.collection.update( <query>, <update>, { multi: true, ... }) 
+*/
--- udemy/mongodb/UpStarMusic/database/queries/SetRetired.js
+++ udemy/mongodb/UpStarMusic/database/queries/SetRetired.js
@@ -5,5 +5,11 @@ const Artist = require('../models/artist');
  * @param {array} _ids - An array of the _id's of of artists to update
  * @return {promise} A promise that resolves after the update
  */
-module.exports = (_ids) => {
-};
+module.exports = (_ids) =>
+  Artist.update(
+    { _id: { $in: _ids } },
+    { $set: { retired: true } },
+    { multi: true },
+    // By default multi is false, so .update only affects the first record. Setting to true means
+    // the query will update ALL records. We could also just use .updateMany
+  );

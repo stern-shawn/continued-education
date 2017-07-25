b6d09ddd86c9671786bb9afacf1f5e01b4101972
Shawn Stern <stern.shawn@gmail.com>
2017-07-24 22:34:15 -0700

    Adding the mongoose queries for finding the min/max age and yearsActive of the Artists collection so we can render the filter sliders accurately
    - Great case for using Promise.all to do both queries at once, and then chain with a .then to make sure it returns an object with the expected keys
    - ES6 destructuring for cleaner code, ie ([min, max]) => ({ min, max }) is way clearer than (result) => return { min: result[0], max: result[1] }

--- udemy/mongodb/UpStarMusic/database/queries/GetAgeRange.js
+++ udemy/mongodb/UpStarMusic/database/queries/GetAgeRange.js
@@ -6,4 +6,22 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+  // .sort ascending + limiting to the first value is the performant way to find min
+  const minQuery = Artist
+    .find()
+    .sort({ age: 1 })
+    .limit(1) // Even with limit(1), result will still be an array, pluck out element 0
+    .then((artists) => artists[0].age);
+
+  // Same as min, just sort in descending order instead
+  const maxQuery = Artist
+    .find()
+    .sort({ age: -1 })
+    .limit(1)
+    .then((artists) => artists[0].age);
+
+  // API is expected to _return a promise_ that resolves to an object with min/max keys
+  // Great time to use ES6 destructuring goodness because the instructor's code is gross
+  return Promise.all([minQuery, maxQuery])
+    .then(([min, max]) => ({ min, max }));
 };
--- udemy/mongodb/UpStarMusic/database/queries/GetYearsActiveRange.js
+++ udemy/mongodb/UpStarMusic/database/queries/GetYearsActiveRange.js
@@ -6,4 +6,22 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+  // .sort ascending + limiting to the first value is the performant way to find min
+  const minQuery = Artist
+    .find()
+    .sort({ yearsActive: 1 })
+    .limit(1)
+    .then((artists) => artists[0].yearsActive);
+
+  // Same as min, just sort in descending order instead
+  const maxQuery = Artist
+    .find()
+    .sort({ yearsActive: -1 })
+    .limit(1)
+    .then((artists) => artists[0].yearsActive);
+
+  // API is expected to _return a promise_ that resolves to an object with min/max keys
+  // Great time to use ES6 destructuring goodness because the instructor's code is gross
+  return Promise.all([minQuery, maxQuery])
+    .then(([min, max]) => ({ min, max }));
 };

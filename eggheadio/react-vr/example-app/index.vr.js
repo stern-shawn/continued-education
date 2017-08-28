c394357ca18892cf4df0e77b7585fd9867bb361a
Shawn Stern <Shawn.Stern@nwea.org>
2017-08-28 16:33:26 -0700

    Experimenting with adding Text components and making use of ReactVR specific style attributes like textAlignVertical and layoutOrigin

--- eggheadio/react-vr/example-app/index.vr.js
+++ eggheadio/react-vr/example-app/index.vr.js
@@ -11,13 +11,22 @@ export default class app extends React.Component {
   render() {
     return (
       <View>
-        <Pano
-            source={asset('chess-world.jpg')}
-            onLoad={() => { console.log('Loaded') }}
-            style={{
-              transform: [{ rotateY: -45 }],
-            }}
-        />
+        <Pano source={asset('chess-world.jpg')} />
+        <Text
+          style={{
+            transform: [{ translateZ: -1 }],
+            width: 0.5,
+            height: 0.45,
+            color: 'lightblue',
+            backgroundColor: 'green',
+            textAlign: 'center',
+            fontWeight: '300',
+            textAlignVertical: 'center',
+            layoutOrigin: [0.5, 0.5],
+          }}
+        >
+        Hello, VR World!
+        </Text>
       </View>
     );
   }

aee9ba0ec421755fc9fc82b2dc0a38ca90663e13
Shawn Stern <Shawn.Stern@nwea.org>
2017-08-29 09:11:35 -0700

    Adding images and text to the view and using flexbox properties for arrangement within Views!

--- eggheadio/react-vr/example-app/index.vr.js
+++ eggheadio/react-vr/example-app/index.vr.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   AppRegistry,
   asset,
+  Image,
   Pano,
   Text,
   View,
@@ -12,21 +13,51 @@ export default class app extends React.Component {
     return (
       <View>
         <Pano source={asset('chess-world.jpg')} />
-        <Text
+        <View
           style={{
-            transform: [{ translateZ: -1 }],
-            width: 0.5,
-            height: 0.45,
-            color: 'lightblue',
-            backgroundColor: 'green',
-            textAlign: 'center',
-            fontWeight: '300',
-            textAlignVertical: 'center',
+            width: 2,
+            height: 2.4,
+            backgroundColor: 'white',
             layoutOrigin: [0.5, 0.5],
+            transform: [{ translate: [0, 0, -3] }],
+            justifyContent: 'space-between',
           }}
         >
-        Hello, VR World!
-        </Text>
+          <Image
+            source={asset('mountains.jpg')}
+            style={{ height: 1.2 }}
+          />
+          <Text
+            style={{
+              color: '#333',
+              fontSize: 0.16,
+              textAlign: 'center',
+            }}
+          >
+            Explore these sick mountains!
+          </Text>
+          <View
+            style={{ flexDirection: 'row' }}
+          >
+            <Image
+              source={asset('thumb1.jpg')}
+              style={{ width: 0.5, height: 0.5 }}
+            />
+            <Image
+              source={asset('thumb2.jpg')}
+              style={{ width: 0.5, height: 0.5 }}
+            />
+            <Image
+              source={asset('thumb3.jpg')}
+              style={{ width: 0.5, height: 0.5 }}
+            />
+            <Image
+              source={asset('thumb4.jpg')}
+              style={{ width: 0.5, height: 0.5 }}
+            />
+          </View>
+
+        </View>
       </View>
     );
   }

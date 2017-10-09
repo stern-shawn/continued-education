1b5c001774af857f66e19b28c4b9f05a3bfc05bb
Shawn Stern <stern.shawn@gmail.com>
2017-10-08 20:22:14 -0700

    Adding actions/reducers, some sample data, rendering a story list, and adding action dispatch to UI buttons to load/clear displayed data

--- eggheadio/redux-observable/src/components/Stories.js
+++ eggheadio/redux-observable/src/components/Stories.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { clear, loadStories } from '../actions';
 
 const Stories = (props) => {
   return (
-    <pre>
-      <code>
-        {JSON.stringify(props)}
-      </code>
-    </pre>
+    <div>
+      <button type="button" onClick={props.loadStories}>Load Top 2 Stories</button>
+      <button type="button" onClick={props.clear}>Clear</button>
+      <StoryList {...props} />
+    </div>
   );
 };
 
+const StoryList = ({ items }) => {
+  if (items.length === 0) return null;
+
+  return (
+    <div>
+      {items.map(item => <Story {...item} key={item.id} />)}
+    </div>
+  )
+};
+
+const Story = (props) => <p>{props.title}</p>
+
 const mapState = (state) => state;
 
-export default connect(mapState)(Stories);
+const mapDispatch = (dispatch) => {
+  return {
+    loadStories: () => dispatch(loadStories()),
+    clear: () => dispatch(clear()),
+  }
+};
+
+export default connect(mapState, mapDispatch)(Stories);
--- eggheadio/redux-observable/src/reducers/index.js
+++ eggheadio/redux-observable/src/reducers/index.js
@@ -1,7 +1,30 @@
+import { CLEAR_STORIES, LOAD_STORIES } from '../actions';
+
 const initialState = {
   items: [],
 };
 
+const stories = [
+  {
+    title: "Headline 1",
+    body: "Lorem ipstuff",
+  },
+  {
+    title: "Ayyyy lmao",
+    body: "Generic af",
+  },
+];
+
 export default function storiesReducer(state = initialState, action) {
-  return state;
+  switch(action.type) {
+    case LOAD_STORIES:
+      return {
+        items: stories.slice(),
+      };
+    case CLEAR_STORIES:
+      return {
+        items: [],
+      };
+    default: return state;
+  }
 }

901fff6d22d855b0857e281711664bb74ba980e3
Shawn Stern <stern.shawn@gmail.com>
2017-11-25 14:02:35 +1000

    Adding users field to Company type to support listing users at a company using the GraphQLList type. Fixing circular dependancy issue by making fields a function that returns an object (think closures) to avoid using UserType before it is defined

--- udemy/graphql/users/schema/schema.js
+++ udemy/graphql/users/schema/schema.js
@@ -4,21 +4,30 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
+  GraphQLList,
   GraphQLSchema,
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     description: { type: GraphQLString },
-  },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then(res => res.data);
+      },
+    }
+  }),
 });
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
@@ -32,7 +41,7 @@ const UserType = new GraphQLObjectType({
           .then(res => res.data);
       },
     },
-  },
+  }),
 });
 
 const RootQuery = new GraphQLObjectType({

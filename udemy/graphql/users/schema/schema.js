10edeb599d0a44ce924c14676aa958d3f9b54c1c
Shawn Stern <stern.shawn@gmail.com>
2017-11-25 12:58:46 +1000

    Defining a root query for fetching users, connecting to the graphQL express middleware, and defining a dummy collection of users so I can start seeing some initial results in graphiql and verify this is working

--- udemy/graphql/users/schema/schema.js
+++ udemy/graphql/users/schema/schema.js
@@ -1,9 +1,16 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
+  GraphQLSchema,
 } = graphql;
+
+const users = [
+  { id: '23', firstName: 'Bill', age: 20 },
+  { id: '47', firstName: 'Sam', age: 21},
+];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -12,4 +19,21 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
   },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      },
+    }
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });

9bc3204452bda787418b2fbb459d7db6fa1c9741
Shawn Stern <stern.shawn@gmail.com>
2018-02-16 23:21:09 -0800

    Using graphql HOC to give SongCreate access to the AddSong mutation, which is now a function that can accept a title query variable. On form submit, react invokes this mutation, passing the title in component state

--- udemy/graphql/lyrical-graphql/client/components/SongCreate/index.js
+++ udemy/graphql/lyrical-graphql/client/components/SongCreate/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 class SongCreate extends Component {
   state = {
@@ -7,11 +9,26 @@ class SongCreate extends Component {
 
   handleChange = (e) => this.setState({ title: e.target.value });
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { mutate } = this.props;
+    const { title } = this.state;
+
+    // Invoke the mutate function passed to our component via graphql HOC as props
+    // and pass in the title variable (object keys match so no need to write title: title)
+    // At the moment we only have ONE mutation tied to the component, so it's that simple
+    mutate({
+      variables: {
+        title,
+      },
+    });
+  }
+
   render() {
     return (
       <div>
         <h3>Create a New Song</h3>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>Song Title:</label>
           <input onChange={this.handleChange} value={this.state.title} />
         </form>
@@ -20,4 +37,14 @@ class SongCreate extends Component {
   }
 }
 
-export default SongCreate;
+// Here we can write the mutation as a function of query instead of with a hardcoded title value
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      id
+      title
+    }
+  }
+`;
+
+export default graphql(mutation)(SongCreate);

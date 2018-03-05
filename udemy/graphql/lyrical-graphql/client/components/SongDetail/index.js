689a10a33488886d3ccb7536df3706867a86484c
Shawn Stern <stern.shawn@gmail.com>
2018-03-04 18:22:37 -0800

    Adding LyricList to render song lyrics on detail page

--- udemy/graphql/lyrical-graphql/client/components/SongDetail/index.js
+++ udemy/graphql/lyrical-graphql/client/components/SongDetail/index.js
@@ -3,6 +3,7 @@ import { graphql } from 'react-apollo';
 import fetchSong from '../../queries/fetchSong';
 import { Link } from 'react-router';
 import LyricCreate from '../LyricCreate';
+import LyricList from '../LyricList';
 
 class SongDetail extends Component {
   render() {
@@ -22,6 +23,7 @@ class SongDetail extends Component {
       <div>
         <Link to="/">Back</Link>
         <h3>{song.title}</h3>
+        <LyricList lyrics={song.lyrics} />
         <LyricCreate songId={id} />
       </div>
     )

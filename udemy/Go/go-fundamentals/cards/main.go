985c685a797859a53043263058fe34055d0afde6
Shawn Stern <Shawn.Stern@nwea.org>
2018-04-08 09:02:08 -0700

    Working with basic variable declaration and assignment, declaring fn return types, the Slice type, and basic for loops in Go

---
+++ udemy/Go/go-fundamentals/cards/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Verbose declaration + assignment
+	// var card string = "Ace of Spades"
+
+	// Short syntax for declare + assign + inferred type
+	// card := "Ace of Spades"
+
+	// Create a new slice of strings
+	cards := []string{"Two of Spades", newCard()}
+	cards = append(cards, "Six of Diamonds")
+
+	// Iterate over the index and values of cards
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+// Note that we need to explicitly declare the return data type to avoid compiler errors
+func newCard() string {
+	return "Five of Hearts"
+}

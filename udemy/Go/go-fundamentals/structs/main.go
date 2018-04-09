ffb9c41d3848e814c069289353520284b6b22900
Shawn Stern <Shawn.Stern@nwea.org>
2018-04-08 20:32:55 -0700

    Declaring a new `contactInfo` struct, nesting it within `person`, and verifying that we can initialize a new instance of a person using Golang's syntax. I love the trailing commas

--- udemy/Go/go-fundamentals/structs/main.go
+++ udemy/Go/go-fundamentals/structs/main.go
@@ -4,24 +4,27 @@ import (
 	"fmt"
 )
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 // Declare a `person` struct, think plain object in JS or a dict in Python
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	// Declare a new struct, providing fields in order, yuck
-	// shawn := person{"Shawn", "Stern"}
+	shawn := person{
+		firstName: "Shawn",
+		lastName:  "Stern",
+		contact: contactInfo{
+			email:   "shawn@nicetryscrapers.com",
+			zipCode: 90210,
+		},
+	}
 
-	// Declare a new struct more explicitly ala JavaScript
-	// shawn := person{firstName: "Shawn", lastName: "Stern"}
-
-	// Third method of struct declaration, values are defaulted to their type's `Zero Value`
-	var shawn person
-	shawn.firstName = "Shawn"
-	shawn.lastName = "Stern"
-
-	fmt.Println(shawn)
-	fmt.Printf("%+v\n", shawn)
+	fmt.Printf("%+v", shawn)
 }

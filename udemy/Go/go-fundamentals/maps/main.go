84830d7d3749368e49179d35168971bf8b081f39
Shawn Stern <Shawn.Stern@nwea.org>
2018-04-08 22:31:04 -0700

    Writing very simple fn for iterating over and printing the k/v pairs in a map using for loop + range of map

--- udemy/Go/go-fundamentals/maps/main.go
+++ udemy/Go/go-fundamentals/maps/main.go
@@ -7,20 +7,27 @@ import (
 func main() {
 	// var colors map[string]string
 
-	colors := make(map[string]string)
+	// colors := make(map[string]string)
 
 	// We always have to use braces when assigning new keys to a map in Go
-	colors["red"] = "#ff0000"
-	colors["white"] = "#ffffff"
-	colors["blue"] = "#0000ff"
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#00ff00",
-	// 	"blue":  "#0000ff",
-	// }
+	// colors["red"] = "#ff0000"
+	// colors["white"] = "#ffffff"
+	// colors["blue"] = "#0000ff"
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"blue":  "#0000ff",
+		"white": "#ffffff",
+	}
 
-	delete(colors, "blue")
+	// delete(colors, "blue")
 
-	fmt.Println(colors)
+	printMap(colors)
 
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }

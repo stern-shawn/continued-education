3383a4256d91ebdfd645e066bc0d6c8e267e7d1a
Shawn Stern <stern.shawn@gmail.com>
2018-04-10 23:01:02 -0700

    Adding some notes on the fact that Node isn't fully single-threaded, and writing a small file that calls a Node fn that operates on multiple threads in the C++ side of Node to verify

---
+++ udemy/Node/advanced-concepts/node-internals/threads.js
@@ -0,0 +1,21 @@
+const crypto = require('crypto');
+
+// Note that start is exactly the same between all calls to crypto, all calls are operating in parallel
+const start = Date.now();
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('1:', Date.now() - start);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('2:', Date.now() - start);
+});
+
+// If Node was fully single-threaded and each call takes ~800ms on this machine,
+// if both happened in sequence in a single thread, total op time should be 1600ms.
+// Instead, both operations execute in ~800ms with a slight duration difference as one was launched
+// just slightly before the other
+
+// This is because pbkdf2 when run on Node's C++ side is passed to libuv, which does the operations
+// in a `Thread Pool` of 4 threads that can handle expensive calculations
+

7fa758fb8163584edd2e9ceb4df57e6ac8a52db3
Shawn Stern <Shawn.Stern@nwea.org>
2018-04-12 11:28:55 -0700

    Setting up some basic clustering, demonstrating that we can process multiple requests in parallel in child clusters while a heavy blocking operation is in process

--- udemy/Node/advanced-concepts/node-performance/index.js
+++ udemy/Node/advanced-concepts/node-performance/index.js
@@ -1,14 +1,31 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-function doWork(duration) {
-  const start = Date.now();
-  while(Date.now() - start < duration) {}
+if (cluster.isMaster) {
+  console.log(cluster.isMaster);
+
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+} else {
+  console.log(cluster.isMaster);
+
+  const express = require('express');
+  const app = express();
+
+  function doWork(duration) {
+    const start = Date.now();
+    while(Date.now() - start < duration) {}
+  }
+
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hi there');
+  });
+
+  app.get('/fast', (req, res) => {
+    res.send('That was fast!');
+  });
+
+  app.listen(3000);
 }
-
-app.get('/', (req, res) => {
-  doWork(5000);
-  res.send('Hi there');
-});
-
-app.listen(3000);

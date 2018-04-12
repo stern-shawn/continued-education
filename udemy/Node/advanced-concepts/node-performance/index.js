f5723fdedd1fe7be0c348cd5824b54f8bcaf2147
Shawn Stern <Shawn.Stern@nwea.org>
2018-04-12 12:41:10 -0700

    Saving manual clustering work as a new file, working with pm2 and webworker-threads module

--- udemy/Node/advanced-concepts/node-performance/index.js
+++ udemy/Node/advanced-concepts/node-performance/index.js
@@ -1,26 +1,17 @@
-// Every CHILD will be restricted to one thread
-process.env.UV_THREADPOOL_SIZE = 1;
-
-const cluster = require('cluster');
 const crypto = require('crypto');
 
-if (cluster.isMaster) {
-  cluster.fork();
-  cluster.fork();
-} else {
-  const express = require('express');
-  const app = express();
+const express = require('express');
+const app = express();
 
 
-  app.get('/', (req, res) => {
-    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-      res.send('Hi there');
-    });
+app.get('/', (req, res) => {
+  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    res.send('Hi there');
   });
+});
 
-  app.get('/fast', (req, res) => {
-    res.send('That was fast!');
-  });
+app.get('/fast', (req, res) => {
+  res.send('That was fast!');
+});
 
-  app.listen(3000);
-}
+app.listen(3000);

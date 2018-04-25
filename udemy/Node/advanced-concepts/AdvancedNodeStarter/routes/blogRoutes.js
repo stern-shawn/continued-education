79fb4b5e23608ddb35d7b8c50439fb09f80133f1
Shawn Stern <Shawn.Stern@nwea.org>
2018-04-25 15:42:27 -0700

    Implementing a very simple redis cache that stores stringified blog queries keyed by id and returns those instead of querying mongodb directly on every GET request. Not optimized since we initialize the redis connection each time, refactor later...

--- udemy/Node/advanced-concepts/AdvancedNodeStarter/routes/blogRoutes.js
+++ udemy/Node/advanced-concepts/AdvancedNodeStarter/routes/blogRoutes.js
@@ -14,8 +14,29 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis = require('redis');
+    const util = require('util');
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
+    // Promisify the client.get callback-based fn so it'll return a promise (can be async/awaited)
+    client.get = util.promisify(client.get);
+
+    // Check for redis-cached data for this request
+    const cachedBlogs = await client.get(req.user.id);
+
+    // If yes, get immediately from redis and return
+    if (cachedBlogs) {
+      console.log('SERVING CACHED VALUE');
+      // redis only stores strings and numbers, so to return the actual object, we'll need to parse from JSON first
+      return res.send(JSON.parse(cachedBlogs));
+    }
+    // If no, reach out to mongo, get data, return to user and add to cache to speed up subsequent calls
+
     const blogs = await Blog.find({ _user: req.user.id });
 
+    console.log('SERVING FROM MONGODB');
+    // redis only stores strings and numbers, so to store the object we'll need to stringify it first
+    client.set(req.user.id, JSON.stringify(blogs));
     res.send(blogs);
   });
 

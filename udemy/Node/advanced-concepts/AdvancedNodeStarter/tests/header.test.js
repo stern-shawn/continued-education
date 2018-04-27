31e146f2770b0494086201504d268d00c6988eb8
Shawn Stern <Shawn.Stern@nwea.org>
2018-04-26 21:53:43 -0700

    Adding beforeEach and afterEach blocks to automatically generate and close a chromium instance for each test instead of repeating in test bodies

--- udemy/Node/advanced-concepts/AdvancedNodeStarter/tests/header.test.js
+++ udemy/Node/advanced-concepts/AdvancedNodeStarter/tests/header.test.js
@@ -1,11 +1,22 @@
 const puppeteer = require('puppeteer');
 
-test('We can launch a new browser instance', async () => {
-  // Launch Chromium, create a new tab, and navigate it to localhost:3000 where the app is located
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto('localhost:3000');
+// Declare shared vars here so that they can be initialized in beforeEach and accessible from test scopes
+let browser;
+let page;
 
+beforeEach(async () => {
+  // Launch Chromium, create a new tab, and navigate it to localhost:3000 where the app is located
+  browser = await puppeteer.launch({ headless: false });
+  page = await browser.newPage();
+  await page.goto('localhost:3000');
+});
+
+// Exit the current instance of Chromium after each test
+afterEach(async () => {
+  await browser.close();
+});
+
+test('We can launch a new browser instance', async () => {
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
 
   expect(text).toEqual('Blogster');

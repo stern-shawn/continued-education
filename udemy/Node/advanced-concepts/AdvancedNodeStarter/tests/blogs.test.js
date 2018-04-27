bfe3ef94cb6f96163bc5d55f2b5a754c966ce4f6
Shawn Stern <Shawn.Stern@nwea.org>
2018-04-27 15:31:07 -0700

    Refactoring common 'When logged in' setup code into a jest describe block to help group related tests in the same context

--- udemy/Node/advanced-concepts/AdvancedNodeStarter/tests/blogs.test.js
+++ udemy/Node/advanced-concepts/AdvancedNodeStarter/tests/blogs.test.js
@@ -11,27 +11,15 @@ afterEach(async () => {
   await page.close();
 });
 
-test('When logged in, can see the blog creation form', async () => {
-  await page.login();
-  await page.click('a.btn-floating.btn-large.red');
+describe('When logged in', async () => {
+  beforeEach(async () => {
+    await page.login();
+    await page.click('a.btn-floating.btn-large.red');
+  });
 
-  const label = await page.getContentsOf('form label');
+  test('can see the blog creation form', async () => {
+    const label = await page.getContentsOf('form label');
 
-  expect(label).toEqual('Blog Title');
+    expect(label).toEqual('Blog Title');
+  });
 });
-
-// test('Clicking login initiates OAuth flow', async () => {
-//   await page.click('.right a');
-
-//   const url = await page.url();
-
-//   expect(url).toMatch(/accounts\.google\.com/);
-// });
-
-// test('When signed in, logout button is displayed', async () => {
-//   // Mock the process of logging in before rest of test
-//   await page.login();
-//   const logoutText = await page.getContentsOf('a[href="/auth/logout"]');
-
-//   expect(logoutText).toEqual('Logout');
-// });

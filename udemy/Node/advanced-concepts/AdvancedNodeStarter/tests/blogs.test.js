91a396abbda777ebd7e0402cadcbd4e124822b97
Shawn Stern <Shawn.Stern@nwea.org>
2018-04-27 15:38:30 -0700

    Adding a nested describe block to handle testing when logged in AND when giving the form invalid (no) input

--- udemy/Node/advanced-concepts/AdvancedNodeStarter/tests/blogs.test.js
+++ udemy/Node/advanced-concepts/AdvancedNodeStarter/tests/blogs.test.js
@@ -22,4 +22,18 @@ describe('When logged in', async () => {
 
     expect(label).toEqual('Blog Title');
   });
+
+  describe('and using invalid inputs', async () => {
+    beforeEach(async () => {
+      await page.click('button[type="submit"]');
+    });
+
+    test('the form shows an error message', async () => {
+      const titleError = await page.getContentsOf('.title .red-text');
+      const contentError = await page.getContentsOf('.content .red-text');
+
+      expect(titleError).toEqual('You must provide a value');
+      expect(contentError).toEqual('You must provide a value');
+    });
+  });
 });

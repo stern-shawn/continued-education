4ab93612203f54113806e602d6e3c2b284ffced5
Shawn Stern <Shawn.Stern@nwea.org>
2018-04-26 21:48:09 -0700

    Updating basic test so that Chromium navigates to the app, gets the innherHTML of the Header logo, and asserts its value using the puppeteer $eval fn in place of document.querySelector

--- udemy/Node/advanced-concepts/AdvancedNodeStarter/tests/header.test.js
+++ udemy/Node/advanced-concepts/AdvancedNodeStarter/tests/header.test.js
@@ -1,6 +1,12 @@
 const puppeteer = require('puppeteer');
 
 test('We can launch a new browser instance', async () => {
+  // Launch Chromium, create a new tab, and navigate it to localhost:3000 where the app is located
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
+  await page.goto('localhost:3000');
+
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+  expect(text).toEqual('Blogster');
 })

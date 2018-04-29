265203c42a0d7b0df7f28c965f1375a32402d184
Shawn Stern <Shawn.Stern@nwea.org>
2018-04-29 15:13:30 -0700

    Adding extra test to verify user cannot access blog posts when not logged in

--- udemy/Node/advanced-concepts/AdvancedNodeStarter/tests/blogs.test.js
+++ udemy/Node/advanced-concepts/AdvancedNodeStarter/tests/blogs.test.js
@@ -79,4 +79,16 @@ describe('When not logged in', async () => {
 
     expect(result).toEqual({ error: 'You must log in!' });
   });
+
+  test('User cannot get a list of blog posts', async () => {
+    const result = await page.evaluate(
+      () => fetch('/api/blogs', {
+        method: 'GET',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/json' },
+      }).then(res => res.json()),
+    );
+
+    expect(result).toEqual({ error: 'You must log in!' });
+  });
 });

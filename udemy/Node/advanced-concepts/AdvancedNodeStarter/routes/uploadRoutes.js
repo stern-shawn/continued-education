57a6382d11772d20d1a2401368083813ab536a42
Shawn Stern <Shawn.Stern@nwea.org>
2018-04-30 12:27:23 -0700

    Promisifying the getSignedUrl operation, because using callbacks goes against the Promise based methodology in the rest of this codebase

--- udemy/Node/advanced-concepts/AdvancedNodeStarter/routes/uploadRoutes.js
+++ udemy/Node/advanced-concepts/AdvancedNodeStarter/routes/uploadRoutes.js
@@ -9,17 +9,25 @@ const s3 = new aws.S3({
   secretAccessKey: keys.secretAccessKey,
 });
 
+// Promisify the getSignedUrl operation, since AWS doesn't have native Promise support for operations that don't return
+// a Result object (getSignedUrl is one of them, yay...)
+const getSignedUrlPromise = (operation, params) =>
+  new Promise((resolve, reject) => {
+    s3.getSignedUrl(operation, params, (err, url) => {
+      err ? reject(err) : resolve(url);
+    });
+  });
+
 module.exports = app => {
   app.get('/api/upload', requireLogin, async (req, res) => {
     // Generate a signed url from aws s3 that our user can use to upload the file directly to s3 with, without us having
     // to temporarily store files in the express server memory or server local disk!
-    s3.getSignedUrl('putObject',
-      {
-        Bucket: 'shawn-stern-advanced-node',
-        ContentType: 'jpeg',
-        Key: `${req.user.id}/${uuid()}.jpeg`,
-      },
-      (err, url) => res.send({ url }),
-    );
+    const url = await getSignedUrlPromise('putObject', {
+      Bucket: 'shawn-stern-advanced-node',
+      ContentType: 'jpeg',
+      Key: `${req.user.id}/${uuid()}.jpeg`,
+    }).catch(err => console.error(err));
+
+    res.send({ url });
   });
 };

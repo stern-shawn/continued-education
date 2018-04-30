c3a7747ba00ddc8165df520f75bdedada4a374e7
Shawn Stern <Shawn.Stern@nwea.org>
2018-04-30 13:43:00 -0700

    Updating font-end client to display the imageUrl if defined for a given blog post

--- udemy/Node/advanced-concepts/AdvancedNodeStarter/client/src/components/blogs/BlogShow.js
+++ udemy/Node/advanced-concepts/AdvancedNodeStarter/client/src/components/blogs/BlogShow.js
@@ -7,6 +7,12 @@ class BlogShow extends Component {
     this.props.fetchBlog(this.props.match.params._id);
   }
 
+  renderImage() {
+    if (this.props.blog.imageUrl) {
+      return <img src={`https://s3-us-west-2.amazonaws.com/shawn-stern-advanced-node/${this.props.blog.imageUrl}`} />;
+    }
+  }
+
   render() {
     if (!this.props.blog) {
       return '';
@@ -18,6 +24,7 @@ class BlogShow extends Component {
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        {this.renderImage()}
       </div>
     );
   }

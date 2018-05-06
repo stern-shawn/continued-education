a6a809a7ce1848d5f29462d9b8561ddcc0118fb8
Shawn Stern <stern.shawn@gmail.com>
2018-05-06 16:43:29 -0700

    Updating app test to work with the fully built version of the app since I did this out of order...

--- udemy/React-Redux/auth-client/test/components/app_test.js
+++ udemy/React-Redux/auth-client/test/components/app_test.js
@@ -3,9 +3,17 @@ import App from '../../src/components/app';
 
 describe('App' , () => {
   let component;
+  // Dummy auth state since application state needs to be mocked to get App to render without reference errors
+  let auth = {
+    authenticated: false,
+    userProfile: {
+      firstName: 'John',
+      lastName: 'Doe',
+    },
+  };
 
   beforeEach(() => {
-    component = renderComponent(App);
+    component = renderComponent(App, {}, { auth });
   });
 
   it('renders something', () => {

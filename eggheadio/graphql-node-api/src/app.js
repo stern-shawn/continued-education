fba0c1c6b35072d14a7e29f59f74628710aa8e4b
Shawn Stern <stern.shawn@gmail.com>
2018-06-13 23:52:00 -0700

    Adding basic dependencies and code to start an express server with graphiql

--- eggheadio/graphql-node-api/src/app.js
+++ eggheadio/graphql-node-api/src/app.js
@@ -1,2 +1,23 @@
-const start = 'Welcome to node-starter-kit';
-console.log(start);
+import express from 'express';
+import graphqlHttp from 'express-graphql';
+
+const app = express();
+const port = 3000;
+
+// Empty object, build later
+const schema = {};
+
+app.use('/graphql', graphqlHttp({
+    graphiql: true,
+    schema,
+}));
+
+app.get('/', (req, res) => {
+    return res.json({
+        msg: 'hello graphql',
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at port: ${port}`);
+});

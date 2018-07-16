b6beb7c531782927427f564c28390f477c94026a
Shawn Stern <shawn.stern@nike.com>
2018-07-16 10:10:17 -0700

    Initial commit of redux observable + recompose form experiment, adding rxjs, redux-observable, recompose, ramda, and autodux to boilerplate

---
+++ others/redux-observable-recompose-signup/src/store.js
@@ -0,0 +1,8 @@
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
+import rootReducer from '/ducks/root';
+
+export default createStore(
+	rootReducer,
+	applyMiddleware(logger)
+);

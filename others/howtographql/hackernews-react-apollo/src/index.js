a6a4732843f611579af287fa56951da749d81ed3
Shawn Stern <stern.shawn@gmail.com>
2018-09-03 23:02:38 -0700

    Updating react app to include apollo bindings, bringing in server code to enable the data layer needed for CRUDing with Prisma

--- others/howtographql/hackernews-react-apollo/src/index.js
+++ others/howtographql/hackernews-react-apollo/src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();

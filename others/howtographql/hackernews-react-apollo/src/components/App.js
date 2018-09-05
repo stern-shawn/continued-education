444a397b98ad939840f31883c4a4a33eb5006057
Shawn Stern <shawn.stern@nike.com>
2018-09-05 11:15:08 -0700

    Adding Search functionality, using the withApollo wrapper to give us access to the client instance via props and invoke it manually to query links using a filter (provided by the pre-built backend)

--- others/howtographql/hackernews-react-apollo/src/components/App.js
+++ others/howtographql/hackernews-react-apollo/src/components/App.js
@@ -4,6 +4,7 @@ import CreateLink from './CreateLink'
 import Header from './Header'
 import LinkList from './LinkList'
 import Login from './Login'
+import Search from './Search'
 
 const App = () => (
   <div className="center w85">
@@ -13,6 +14,7 @@ const App = () => (
         <Route exact path="/" component={LinkList} />
         <Route exact path="/create" component={CreateLink} />
         <Route exact path="/login" component={Login} />
+        <Route exact path='/search' component={Search} />
       </Switch>
     </div>
   </div>

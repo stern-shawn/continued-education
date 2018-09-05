985206e72283ee630993164b7583b5f833cdf469
Shawn Stern <stern.shawn@gmail.com>
2018-09-04 22:48:45 -0700

    Adding post mutation in the CreateLink component using a basic form + button wrapped in react-apollo's Mutation helper + render prop. Bringing in react-router to enable a header for navigation, and adding redirect to link list upon successful post completion

--- others/howtographql/hackernews-react-apollo/src/components/App.js
+++ others/howtographql/hackernews-react-apollo/src/components/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import CreateLink from './CreateLink'
+import Header from './Header'
 import LinkList from './LinkList'
 
-class App extends Component {
-  render() {
-    return <LinkList />
-  }
-}
+const App = () => (
+  <div className="center w85">
+    <Header />
+    <div className="ph3 pv1 background-gray">
+      <Switch>
+        <Route exact path="/" component={LinkList} />
+        <Route exact path="/create" component={CreateLink} />
+      </Switch>
+    </div>
+  </div>
+)
 
 export default App
---
+++ others/howtographql/hackernews-react-apollo/src/components/CreateLink.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const POST_MUTATION = gql`
+  mutation PostMutation($description: String!, $url: String!) {
+    post(description: $description, url: $url) {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`
+
+class CreateLink extends Component {
+  state = {
+    description: '',
+    url: '',
+  }
+
+  changeDescription = e => this.setState({ description: e.target.value })
+
+  changeUrl = e => this.setState({ url: e.target.value })
+
+  navigateOnComplete = () => this.props.history.push('/')
+
+  render() {
+    const { description, url } = this.state
+
+    return (
+      <div>
+        <div className="flex flex-column mt3">
+          <input
+            className="mb2"
+            value={description}
+            onChange={this.changeDescription}
+            type="text"
+            placeholder="A description for the link"
+          />
+          <input
+            className="mb2"
+            value={url}
+            onChange={this.changeUrl}
+            type="text"
+            placeholder="The URL for the link"
+          />
+        </div>
+        <Mutation
+          mutation={POST_MUTATION}
+          variables={{ description, url }}
+          onCompleted={this.navigateOnComplete}
+        >
+          {postMutation => <button onClick={postMutation}>Submit</button>}
+        </Mutation>
+      </div>
+    )
+  }
+}
+
+export default CreateLink

6323ccb347f42a91396fdd267af40c55870bacf2
Shawn Stern <shawn.stern@nike.com>
2018-12-03 16:14:44 -0800

    Adding ability to toggle/delete todos w/ live subscription updates between multiple browsers/users, whoa AppSync 🎉

--- others/aws/AppSync/todo-app/src/App.js
+++ others/aws/AppSync/todo-app/src/App.js
@@ -28,9 +28,9 @@ const CREATE_TODO = gql`
   }
 `;
 
-const SUBSCRIBE_TODOS = gql`
-  subscription {
-    onCreateTodo {
+const DELETE_TODO = gql`
+  mutation($id: ID!) {
+    deleteTodo(input: { id: $id }) {
       id
       title
       completed
@@ -38,7 +38,44 @@ const SUBSCRIBE_TODOS = gql`
   }
 `;
 
-const App = ({ createTodo, subscribeToMore, todos }) => {
+const UPDATE_TODO = gql`
+  mutation($id: ID!, $completed: Boolean, $title: String) {
+    updateTodo(input: { id: $id, completed: $completed, title: $title }) {
+      id
+      title
+      completed
+    }
+  }
+`;
+
+const SUBSCRIBE_TODOS = gql`
+  subscription {
+    onCreateTodo {
+      id
+      title
+      completed
+    }
+    onUpdateTodo {
+      id
+      title
+      completed
+    }
+    onDeleteTodo {
+      id
+      title
+      completed
+    }
+  }
+`;
+
+const App = ({
+  client,
+  createTodo,
+  deleteTodo,
+  todos,
+  subscribeToMore,
+  updateTodo,
+}) => {
   const [todoInput, setTodoInput] = useState('');
 
   // Subscribe to graphQL subscriptions from AWS AppSync for live updates to todos across clients
@@ -60,15 +97,24 @@ const App = ({ createTodo, subscribeToMore, todos }) => {
     setTodoInput('');
   };
 
+  const toggleComplete = item =>
+    updateTodo({
+      ...item,
+      completed: !item.completed,
+    });
+
+  const removeTodo = (e, { id }) => {
+    e.stopPropagation();
+    deleteTodo({ id });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Todos</p>
       </header>
-      <form onSubmit={addTodo}>
+      <form onSubmit={addTodo} style={{ marginTop: '1rem' }}>
         <input
           onChange={e => setTodoInput(e.target.value)}
           value={todoInput}
@@ -77,10 +123,24 @@ const App = ({ createTodo, subscribeToMore, todos }) => {
         <button type="submit">Add Todo</button>
       </form>
       {todos.length > 0 && (
-        <ul style={{ listStyleType: 'none' }}>
+        <ul
+          style={{
+            listStyleType: 'none',
+            display: 'flex',
+            flexDirection: 'column',
+            placeItems: 'center',
+          }}
+        >
           {todos.map(item => (
-            <li key={item.id} style={{ marginBottom: '.25rem' }}>
-              {item.title}
+            <li
+              key={item.id}
+              style={{
+                marginBottom: '.25rem',
+                textDecoration: item.completed && 'line-through',
+              }}
+              onClick={() => toggleComplete(item)}
+            >
+              {item.title} <button onClick={e => removeTodo(e, item)}>x</button>
             </li>
           ))}
         </ul>
@@ -91,6 +151,8 @@ const App = ({ createTodo, subscribeToMore, todos }) => {
 
 export default compose(
   graphqlMutation(CREATE_TODO, LIST_TODOS, 'Todo'),
+  graphqlMutation(DELETE_TODO, LIST_TODOS, 'Todo'),
+  graphqlMutation(UPDATE_TODO, LIST_TODOS, 'Todo'),
   graphql(LIST_TODOS, {
     options: {
       fetchPolicy: 'cache-and-network',

fa78bc48f8b989e3aefa9e0fb662facbbccbbb14
stern-shawn <stern.shawn@gmail.com>
2019-01-20 23:15:20 -0800

    Cleaning up root app components

--- others/aws/AppSync/photo-albums/src/App.js
+++ others/aws/AppSync/photo-albums/src/App.js
@@ -2,22 +2,19 @@ import React from 'react'
 import Amplify from '@aws-amplify/core'
 import awsExports from './aws-exports'
 import { withAuthenticator } from 'aws-amplify-react'
-import { Grid, Header } from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react'
 import AlbumsListLoader from './components/AlbumsListLoader'
 import NewAlbum from './components/NewAlbum'
 
 Amplify.configure(awsExports)
 
 const App = () => (
-  <div>
-    <Header as="h1">Hello World! I'm a photo app!</Header>
-    <Grid padded>
-      <Grid.Column>
-        <NewAlbum />
-        <AlbumsListLoader />
-      </Grid.Column>
-    </Grid>
-  </div>
+  <Grid padded>
+    <Grid.Column>
+      <NewAlbum />
+      <AlbumsListLoader />
+    </Grid.Column>
+  </Grid>
 )
 
 export default withAuthenticator(App, { includeGreetings: true })

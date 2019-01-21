c8e4a88f1e45cb08ff50ef4c1910624f029c471b
stern-shawn <stern.shawn@gmail.com>
2019-01-20 23:11:27 -0800

    Adding real-time subscriptions to the AlbumsListLoader so component will concatenate any new album items as the API reports them and rerender

--- others/aws/AppSync/photo-albums/src/components/AlbumsListLoader.js
+++ others/aws/AppSync/photo-albums/src/components/AlbumsListLoader.js
@@ -12,15 +12,47 @@ const ListAlbums = `query ListAlbums {
   }
 }`
 
-const AlbumsListLoader = () => (
-  <Connect query={graphqlOperation(ListAlbums)}>
-    {({ data, loading, errors }) => {
-      if (loading) return <div>Loading...</div>
-      if (!data.listAlbums) return
+const SubscribeToNewAlbums = `
+  subscription OnCreateAlbum {
+    onCreateAlbum {
+      id
+      name
+    }
+  }
+`
 
-      return <AlbumsList albums={data.listAlbums.items} />
-    }}
-  </Connect>
-)
+class AlbumsListLoader extends React.Component {
+  onNewAlbum = (prevQuery, newData) => {
+    // When we get data about a new album, we need to put in into an object with the same shape as the original query
+    // results, but with the new data merged in
+    const updatedQuery = {
+      ...prevQuery,
+      listAlbums: {
+        items: [...prevQuery.listAlbums.items, newData.onCreateAlbum],
+      },
+    }
+    return updatedQuery
+  }
+
+  render() {
+    return (
+      <Connect
+        query={graphqlOperation(ListAlbums)}
+        // Listen to SubscribeToNewAlbums subscription
+        subscription={graphqlOperation(SubscribeToNewAlbums)}
+        // Handle new subscription messages
+        onSubscriptionMsg={this.onNewAlbum}
+      >
+        {({ data, loading, errors }) => {
+          if (loading) return <div>Loading...</div>
+          if (errors.length > 0) return <div>{JSON.stringify(errors)}</div>
+          if (!data.listAlbums) return null
+
+          return <AlbumsList albums={data.listAlbums.items} />
+        }}
+      </Connect>
+    )
+  }
+}
 
 export default AlbumsListLoader

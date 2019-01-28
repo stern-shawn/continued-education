0e6e427f1771754d3f424481f99143521fd6d008
stern-shawn <stern.shawn@gmail.com>
2019-01-27 22:54:29 -0800

    Adding sort index to photos table and load more button + state handling to AlbumDetailsLoader for albums with 10+ photos
    - todo: this should be doable with react apollo fetchmore, etc instead of this funk from the tutorial

--- others/aws/AppSync/photo-albums/src/components/AlbumDetails.js
+++ others/aws/AppSync/photo-albums/src/components/AlbumDetails.js
@@ -1,21 +1,23 @@
 import React from 'react'
-import { Connect, S3Image } from 'aws-amplify-react'
-import { graphqlOperation } from 'aws-amplify'
-import { Divider, Header, Segment } from 'semantic-ui-react'
+import { S3Image } from 'aws-amplify-react'
+import { API, graphqlOperation } from 'aws-amplify'
+import { Divider, Form, Header, Segment } from 'semantic-ui-react'
 import S3ImageUpload from './S3ImageUpload'
 
 // todo: add fullsize to query and support fullsize detail modal/route?
-const GetAlbum = `query GetAlbum($id: ID!) {
+const GetAlbum = `query GetAlbum($id: ID!, $nextTokenForPhotos: String) {
   getAlbum(id: $id) {
     id
     name
-    photos {
+    photos(sortDirection: DESC, nextToken: $nextTokenForPhotos) {
+      nextToken
       items {
         thumbnail {
+          width
+          height
           key
         }
       }
-      nextToken
     }
   }
 }`
@@ -33,24 +35,71 @@ const PhotosList = ({ photos }) => (
   </>
 )
 
-const AlbumDetails = ({ album }) => (
-  <Segment>
-    <Header as="h3">{album.name}</Header>
-    <S3ImageUpload albumId={album.id} />
-    <PhotosList photos={album.photos.items} />
-  </Segment>
-)
+const AlbumDetails = ({ album, hasMorePhotos, loadMorePhotos, loadingPhotos }) =>
+  !album ? (
+    'Loading Album...'
+  ) : (
+    <Segment>
+      <Header as="h3">{album.name}</Header>
+      <S3ImageUpload albumId={album.id} />
+      <PhotosList photos={album.photos.items} />
+      {hasMorePhotos && (
+        <Form.Button
+          onClick={loadMorePhotos}
+          icon="refresh"
+          disabled={loadingPhotos}
+          content={loadingPhotos ? 'Loading...' : 'Load more photos'}
+        />
+      )}
+    </Segment>
+  )
 
-const AlbumDetailsLoader = ({ id }) => (
-  <Connect query={graphqlOperation(GetAlbum, { id })}>
-    {({ data, loading, errors }) => {
-      if (loading) return <div>Loading...</div>
-      if (errors.length > 0) return <div>{JSON.stringify(errors)}</div>
-      if (!data.getAlbum) return
+class AlbumDetailsLoader extends React.Component {
+  state = {
+    nextTokenForPhotos: null,
+    hasMorePhotos: true,
+    album: null,
+    loading: true,
+  }
 
-      return <AlbumDetails album={data.getAlbum} />
-    }}
-  </Connect>
-)
+  componentDidMount() {
+    this.loadMorePhotos()
+  }
+
+  loadMorePhotos = async () => {
+    if (!this.state.hasMorePhotos) return
+
+    this.setState({ loading: true })
+    const { data } = await API.graphql(
+      graphqlOperation(GetAlbum, { id: this.props.id, nextTokenForPhotos: this.state.nextTokenForPhotos }),
+    )
+
+    let album
+    if (this.state.album === null) {
+      album = data.getAlbum
+    } else {
+      album = this.state.album
+      album.photos.items = album.photos.items.concat(data.getAlbum.photos.items)
+    }
+
+    this.setState({
+      album,
+      loading: false,
+      nextTokenForPhotos: data.getAlbum.photos.nextToken,
+      hasMorePhotos: data.getAlbum.photos.nextToken !== null,
+    })
+  }
+
+  render() {
+    return (
+      <AlbumDetails
+        loadingPhotos={this.state.loading}
+        album={this.state.album}
+        loadMorePhotos={this.loadMorePhotos}
+        hasMorePhotos={this.state.hasMorePhotos}
+      />
+    )
+  }
+}
 
 export default AlbumDetailsLoader

3bb6c9e606d7bc6c5ae78b817c3079e0f65225c2
stern-shawn <stern.shawn@gmail.com>
2019-05-04 13:02:12 -0700

    Add a draggable handle to each task instead of having the entire element as a Handle

--- eggheadio/react-beautiful-dnd/task-app/src/Task.jsx
+++ eggheadio/react-beautiful-dnd/task-app/src/Task.jsx
@@ -8,17 +8,22 @@ const Container = styled.div`
   border-radius: 2px;
   margin-bottom: 8px;
   padding: 8px;
+  display: flex;
+`
+
+const Handle = styled.div`
+  width: 10px;
+  height: 20px;
+  background-color: darkgrey;
+  border-radius: 4px;
+  margin-right: 8px;
 `
 
 const Task = ({ index, task }) => (
   <Draggable draggableId={task.id} index={index}>
     {(provided, snapshot) => (
-      <Container
-        ref={provided.innerRef}
-        {...provided.draggableProps}
-        {...provided.dragHandleProps}
-        isDragging={snapshot.isDragging}
-      >
+      <Container ref={provided.innerRef} {...provided.draggableProps} isDragging={snapshot.isDragging}>
+        <Handle {...provided.dragHandleProps} />
         {task.content}
       </Container>
     )}

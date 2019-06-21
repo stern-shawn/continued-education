802d6fa838384bd62c6a649226cbb6fea18127d3
stern-shawn <stern.shawn@gmail.com>
2019-06-20 19:09:23 -0700

    Naive LinkedList implementation to show that we can apply generic Sorter to this data type since it implements Sortable

--- udemy/typescript/sort/src/index.ts
+++ udemy/typescript/sort/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter'
 import { NumbersCollection } from './NumbersCollection'
 import { CharactersCollection } from './CharactersCollection'
+import { LinkedList } from './LinkedList'
 
 const numbers = new NumbersCollection([10, -20, 3, -5, 0])
 const numSorter = new Sorter(numbers)
@@ -11,3 +12,13 @@ const characters = new CharactersCollection('XabDShawn')
 const charSorter = new Sorter(characters)
 charSorter.sort()
 console.log(characters.data)
+
+const linkedList = new LinkedList()
+linkedList.add(50)
+linkedList.add(-1)
+linkedList.add(4)
+linkedList.add(-20)
+
+const linkedListSorter = new Sorter(linkedList)
+linkedListSorter.sort()
+linkedList.print()

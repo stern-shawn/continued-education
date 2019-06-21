fb8a6e0e62a14e2459aaae4faeb73627f429e702
stern-shawn <stern.shawn@gmail.com>
2019-06-20 19:27:37 -0700

    Refactoring with Sorter as an abstract class that can be extended so all children get .sort() vs needing to separately instantiate a Sortable class for a collection

--- udemy/typescript/sort/src/Sorter.ts
+++ udemy/typescript/sort/src/Sorter.ts
@@ -1,20 +1,16 @@
-interface Sortable {
-  length: number
-  compare(leftIndex: number, rightIndex: number): boolean
-  swap(leftIndex: number, rightIndex: number): void
-}
-
-export class Sorter {
-  constructor(public collection: Sortable) {}
+export abstract class Sorter {
+  abstract length: number
+  abstract compare(leftIndex: number, rightIndex: number): boolean
+  abstract swap(leftIndex: number, rightIndex: number): void
 
   sort(): void {
-    const { length } = this.collection
+    const { length } = this
 
-    // Nasty nasty bubble sort, but that's not really the point, interfaces/patterns are
+    // Nasty nasty bubble sort, but that's not really the point, interfaces/inheritance/design patterns are
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1)
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1)
         }
       }
     }
--- udemy/typescript/sort/src/index.ts
+++ udemy/typescript/sort/src/index.ts
@@ -1,16 +1,13 @@
-import { Sorter } from './Sorter'
 import { NumbersCollection } from './NumbersCollection'
 import { CharactersCollection } from './CharactersCollection'
 import { LinkedList } from './LinkedList'
 
 const numbers = new NumbersCollection([10, -20, 3, -5, 0])
-const numSorter = new Sorter(numbers)
-numSorter.sort()
+numbers.sort()
 console.log(numbers.data)
 
 const characters = new CharactersCollection('XabDShawn')
-const charSorter = new Sorter(characters)
-charSorter.sort()
+characters.sort()
 console.log(characters.data)
 
 const linkedList = new LinkedList()
@@ -18,7 +15,5 @@ linkedList.add(50)
 linkedList.add(-1)
 linkedList.add(4)
 linkedList.add(-20)
-
-const linkedListSorter = new Sorter(linkedList)
-linkedListSorter.sort()
+linkedList.sort()
 linkedList.print()

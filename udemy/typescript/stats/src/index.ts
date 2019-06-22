1382524fb04746a4b996a051c944c9be614c73d4
stern-shawn <stern.shawn@gmail.com>
2019-06-21 21:32:07 -0700

    Adding static methods which allow us to return pre-configured instances of Readers/Summaries

--- udemy/typescript/stats/src/index.ts
+++ udemy/typescript/stats/src/index.ts
@@ -1,19 +1,7 @@
-import { ConsoleReport } from './reportTargets/ConsoleReport'
-import { CsvFileReader } from './CsvFileReader'
 import { MatchReader } from './MatchReader'
 import { Summary } from './Summary'
-import { WinsAnalysis } from './analyzers/WinsAnalysis'
-import { HtmlReport } from './reportTargets/HtmlReport'
 
-const csvFileReader = new CsvFileReader('football.csv')
-const matchReader = new MatchReader(csvFileReader)
+const matchReader = MatchReader.fromCsv('football.csv')
 matchReader.load()
 
-const manUnitedSummary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport())
-manUnitedSummary.buildAndPrintReport(matchReader.matches)
-
-const liverpoolSummary = new Summary(new WinsAnalysis('Liverpool'), new ConsoleReport())
-liverpoolSummary.buildAndPrintReport(matchReader.matches)
-
-const arsenalSummary = new Summary(new WinsAnalysis('Arsenal'), new HtmlReport('report.html'))
-arsenalSummary.buildAndPrintReport(matchReader.matches)
+Summary.winsAnalysisWithHtmlReport('Arsenal').buildAndPrintReport(matchReader.matches)

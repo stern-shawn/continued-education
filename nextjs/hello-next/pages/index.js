369210f8478398ffff3105c6c90c845b4f014ea2
Shawn Stern <shawn.stern0@walmartlabs.com>
2020-03-28 16:45:47 -0700

    ugly query param routing example

--- nextjs/hello-next/pages/index.js
+++ nextjs/hello-next/pages/index.js
@@ -1,9 +1,23 @@
 import Layout from "../components/MyLayout";
+import Link from "next/link";
 
-const Index = () => (
+const PostLink = ({ title }) => (
+  <li>
+    <Link href={`/post?title=${title}`}>
+      <a>{title}</a>
+    </Link>
+  </li>
+);
+
+const Blog = () => (
   <Layout>
-    <p>Hello Next.js</p>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink title="Hello Next.js" />
+      <PostLink title="Learn Next.js is awesome" />
+      <PostLink title="Deploy apps with Zeit" />
+    </ul>
   </Layout>
 );
 
-export default Index;
+export default Blog;

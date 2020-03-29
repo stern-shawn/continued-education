57745b1599b679d81280a4edda82d90617f0b9c9
Shawn Stern <shawn.stern0@walmartlabs.com>
2020-03-28 17:02:27 -0700

    Introduce getInitialProps for remote data fetching before render

--- nextjs/hello-next/pages/index.js
+++ nextjs/hello-next/pages/index.js
@@ -1,22 +1,31 @@
 import Layout from "../components/MyLayout";
 import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 
-const PostLink = ({ id }) => (
-  <li>
-    <Link href="/p/[id]" as={`/p/${id}`}>
-      <a>{id}</a>
-    </Link>
-  </li>
-);
-
-const Blog = () => (
+const Index = ({ shows }) => (
   <Layout>
-    <h1>My Blog</h1>
+    <h1>Batman TV Shows</h1>
     <ul>
-      <PostLink id="hello-nextjs" />
-      <PostLink id="learn-nextjs" />
-      <PostLink id="deploy-nextjs" />
+      {shows.map(show => (
+        <li key={show.id}>
+          <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
     </ul>
   </Layout>
 );
-export default Blog;
+
+Index.getInitialProps = async () => {
+  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
+
+export default Index;
--- nextjs/hello-next/pages/p/[id].js
+++ nextjs/hello-next/pages/p/[id].js
@@ -1,15 +1,22 @@
-import { useRouter } from "next/router";
 import Layout from "../../components/MyLayout";
+import fetch from "isomorphic-unfetch";
 
-const Post = () => {
-  const router = useRouter();
+const Post = ({ show }) => (
+  <Layout>
+    <h1>{show.name}</h1>
+    <p>{show.summary.replace(/<[/]?[pb]>/g, "")}</p>
+    {show.image ? <img src={show.image.medium} /> : null}
+  </Layout>
+);
 
-  return (
-    <Layout>
-      <h1>{router.query.id}</h1>
-      <p>This is the blog post content.</p>
-    </Layout>
-  );
+Post.getInitialProps = async context => {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  console.log(`Fetched show: ${show.name}`);
+
+  return { show };
 };
 
 export default Post;

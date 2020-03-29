4f9175b7f2148250b963e51943dae919c6abca4e
Shawn Stern <shawn.stern0@walmartlabs.com>
2020-03-28 17:10:54 -0700

    css-in-js styling via next styled-jsx option

--- nextjs/hello-next/pages/index.js
+++ nextjs/hello-next/pages/index.js
@@ -2,18 +2,48 @@ import Layout from "../components/MyLayout";
 import Link from "next/link";
 import fetch from "isomorphic-unfetch";
 
+const ShowLink = ({ show }) => (
+  <li key={show.id}>
+    <Link href="/p/[id]" as={`/p/${show.id}`}>
+      <a>{show.name}</a>
+    </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: "Arial";
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </li>
+);
+
 const Index = ({ shows }) => (
   <Layout>
     <h1>Batman TV Shows</h1>
     <ul>
       {shows.map(show => (
-        <li key={show.id}>
-          <Link href="/p/[id]" as={`/p/${show.id}`}>
-            <a>{show.name}</a>
-          </Link>
-        </li>
+        <ShowLink show={show} />
       ))}
     </ul>
+    <style jsx>{`
+      h1,
+      a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+    `}</style>
   </Layout>
 );
 

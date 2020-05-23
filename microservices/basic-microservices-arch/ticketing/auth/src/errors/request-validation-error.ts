095e1a645b29b931f837a5046e35a9a37590af16
Shawn Stern <stern.shawn@gmail.com>
2020-05-23 15:04:20 -0700

    use abstract CustomError class to enforce standard implementation of errors, simplify err handler middleware using those standards

--- microservices/basic-microservices-arch/ticketing/auth/src/errors/request-validation-error.ts
+++ microservices/basic-microservices-arch/ticketing/auth/src/errors/request-validation-error.ts
@@ -1,10 +1,20 @@
 import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
+  statusCode = 400;
+
   constructor(public errors: ValidationError[]) {
-    super();
+    super('Invalid request params');
 
     // Only because we're extending a built-in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map(({ msg, param }) => ({
+      message: msg,
+      field: param,
+    }));
   }
 }
--- microservices/basic-microservices-arch/ticketing/auth/src/middlewares/error-handler.ts
+++ microservices/basic-microservices-arch/ticketing/auth/src/middlewares/error-handler.ts
@@ -1,20 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   console.log('Something went wrong', err);
-  if (err instanceof RequestValidationError) {
-    const formattedErrors = err.errors.map(({ msg, param }) => ({
-      message: msg,
-      field: param,
-    }));
 
-    return res.status(400).send({ errors: formattedErrors });
-  }
-
-  if (err instanceof DatabaseConnectionError) {
-    return res.status(500).send({ errors: [{ message: err.reason }] });
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   res.status(400).send({ errors: [{ message: 'Something went wrong' }] });

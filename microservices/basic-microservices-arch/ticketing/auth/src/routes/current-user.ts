636c9a31a66be5e53f2d1228d87c8ad4e4caf59e
Shawn Stern <stern.shawn@gmail.com>
2020-05-25 17:39:50 -0700

    implement currentuser route handler/jwt verification + error handling

--- microservices/basic-microservices-arch/ticketing/auth/src/routes/current-user.ts
+++ microservices/basic-microservices-arch/ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,19 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there');
+router.get('/api/users/currentuser', async (req: Request, res: Response) => {
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const currentUser = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser });
+  } catch (err) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };

636a2a8cc2218254c7b7e7c555cbe44dc71fc264
Shawn Stern <stern.shawn@gmail.com>
2020-05-27 21:22:44 -0700

    refactor signin cookie code into global helper

--- microservices/basic-microservices-arch/ticketing/auth/src/test/setup.ts
+++ microservices/basic-microservices-arch/ticketing/auth/src/test/setup.ts
@@ -1,6 +1,17 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
+
 import { app } from '../app';
+import { signupUri } from '../routes/__test__/signup.test';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin: () => Promise<{ cookie: string[]; email: string; password: string }>;
+    }
+  }
+}
 
 let mongo: MongoMemoryServer;
 
@@ -28,3 +39,15 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app).post(signupUri).send({ email, password }).expect(201);
+  // Supertest doesn't automatically manage cookies for us like browsers or postman does, we'll need
+  // to capture it and manually append to requests to mimic working cookies :)
+  const cookie = response.get('Set-Cookie');
+
+  return { cookie, email, password };
+};

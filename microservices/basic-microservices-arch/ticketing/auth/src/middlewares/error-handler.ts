3a0ba98222acdbe499a514f02a437e52039fce76
Shawn Stern <stern.shawn@gmail.com>
2020-05-27 20:50:39 -0700

    add more assertions around signup route handler

--- microservices/basic-microservices-arch/ticketing/auth/src/middlewares/error-handler.ts
+++ microservices/basic-microservices-arch/ticketing/auth/src/middlewares/error-handler.ts
@@ -2,8 +2,6 @@ import { Request, Response, NextFunction } from 'express';
 import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.log('Something went wrong', err);
-
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
--- microservices/basic-microservices-arch/ticketing/auth/src/routes/__test__/signup.test.ts
+++ microservices/basic-microservices-arch/ticketing/auth/src/routes/__test__/signup.test.ts
@@ -4,3 +4,17 @@ import { app } from '../../app';
 it('returns a 201 on successful signup', async () => {
   return request(app).post('/api/users/signup').send({ email: 'test@test.com', password: 'password' }).expect(201);
 });
+
+it('returns a 400 for invalid email', async () => {
+  return request(app).post('/api/users/signup').send({ email: 'nowayman', password: 'password' }).expect(400);
+});
+
+it('returns a 400 for invalid password', async () => {
+  return request(app).post('/api/users/signup').send({ email: 'test@test.com', password: 'p' }).expect(400);
+});
+
+it('returns a 400 for missing email and password', async () => {
+  await request(app).post('/api/users/signup').send({}).expect(400);
+  await request(app).post('/api/users/signup').send({ email: 'test@test.com' }).expect(400);
+  return request(app).post('/api/users/signup').send({ password: 'p' }).expect(400);
+});

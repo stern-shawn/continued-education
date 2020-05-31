9e6a0f99bf16dd0c9363297f671b757544dd234c
Shawn Stern <stern.shawn@gmail.com>
2020-05-30 23:30:08 -0700

    add basic nested route with form

---
+++ microservices/basic-microservices-arch/ticketing/client/pages/auth/signup.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(email, password);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1> Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input className="form-control" type="text" value={email} onChange={(event) => setEmail(event.target.value)} />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          className="form-control"
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+};

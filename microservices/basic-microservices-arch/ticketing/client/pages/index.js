6e83c050bbf7ed01452253d7b8e81e7cc6ba7a2a
Shawn Stern <stern.shawn@gmail.com>
2020-05-31 12:51:00 -0700

    initial, messy getInitialProps which manually determines if it should reach to k8s ingress directly for SSR or if we are in the client

--- microservices/basic-microservices-arch/ticketing/client/pages/index.js
+++ microservices/basic-microservices-arch/ticketing/client/pages/index.js
@@ -1,7 +1,27 @@
-import React, { Component } from 'react';
+import axios from 'axios';
 
-export default class index extends Component {
-  render() {
-    return <h1>INDEX</h1>;
+const LandingPage = ({ currentUser }) => {
+  console.log('currentUser: ', currentUser);
+
+  return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        // Passes along host: 'ticketing.dev' as well as our cookie header :D. This is necessary so
+        // that inside k8s, ingress sees a request to ticketing.dev and can apply its routing rules
+        // since we're using a radically different path to hit the ingress LB...
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
   }
-}
+};
+
+export default LandingPage;

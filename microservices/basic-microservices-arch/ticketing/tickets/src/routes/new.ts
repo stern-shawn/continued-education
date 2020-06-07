f1ddd1b633e04cd5cb1d94af74f8f212418ce96a
Shawn Stern <stern.shawn@gmail.com>
2020-06-06 23:04:04 -0700

    add NATS publish event handler to new ticket creation

--- microservices/basic-microservices-arch/ticketing/tickets/src/routes/new.ts
+++ microservices/basic-microservices-arch/ticketing/tickets/src/routes/new.ts
@@ -1,10 +1,15 @@
 import { requireAuth, validateRequest } from '@sstickets/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import nats from 'node-nats-streaming';
 
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
+
+const stan = nats.connect('ticketing', 'tickets', {
+  url: 'http://nats-srv:4222',
+});
 
 router.post(
   '/api/tickets',
@@ -24,6 +29,15 @@ router.post(
     });
 
     await ticket.save();
+
+    const event = {
+      type: 'ticket:created',
+      data: ticket,
+    };
+
+    stan.publish('ticket:created', JSON.stringify(event), () => {
+      console.log('Ticket creation event published');
+    });
 
     res.status(201).send(ticket);
   }

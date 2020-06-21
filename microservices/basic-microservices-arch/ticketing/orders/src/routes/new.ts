20b686d17f24417644e31c4fb08fe849f6a76b06
Shawn Stern <stern.shawn@gmail.com>
2020-06-20 22:42:14 -0700

    chore: initial new order logic

--- microservices/basic-microservices-arch/ticketing/orders/src/routes/new.ts
+++ microservices/basic-microservices-arch/ticketing/orders/src/routes/new.ts
@@ -1,7 +1,10 @@
-import { requireAuth, validateRequest } from '@sstickets/common';
+import { requireAuth, validateRequest, NotFoundError, OrderStatus, BadRequestError } from '@sstickets/common';
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+
+import { Order } from '../models/order';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
@@ -17,6 +20,28 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+    // Find the ticket the user is trying to order
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+    // Make sure it isn't already reserved
+    const existingOrder = await Order.findOne({
+      ticket,
+      status: {
+        $in: [OrderStatus.Created, OrderStatus.AwaitingPayment, OrderStatus.Complete],
+      },
+    });
+    if (existingOrder) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+    // Calculate an exp date so the ticket can free up for other users
+
+    // Build the order and save it
+
+    // Publish an order created event
+
     res.send({});
   }
 );

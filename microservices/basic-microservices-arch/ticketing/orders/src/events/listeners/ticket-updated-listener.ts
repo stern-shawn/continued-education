dd9391767cf2f64e4c4d0b3f29c602e81805c96e
Shawn Stern <stern.shawn@gmail.com>
2020-06-24 22:15:00 -0700

    refactor: find by id + version refactored into Ticket static method helper

--- microservices/basic-microservices-arch/ticketing/orders/src/events/listeners/ticket-updated-listener.ts
+++ microservices/basic-microservices-arch/ticketing/orders/src/events/listeners/ticket-updated-listener.ts
@@ -10,8 +10,7 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
     console.log('Ticket Updated event received by orders service', data);
     const { id, title, price, version } = data;
-    // We only want to process incremental updates to avoid concurrency issues, verify using version
-    const ticket = await Ticket.findOne({ _id: id, version: version - 1 });
+    const ticket = await Ticket.findByEvent({ id, version });
 
     // TODO: Proper error handling in events
     if (!ticket) throw new Error('Ticket not found');
--- microservices/basic-microservices-arch/ticketing/orders/src/models/ticket.ts
+++ microservices/basic-microservices-arch/ticketing/orders/src/models/ticket.ts
@@ -18,6 +18,7 @@ export interface TicketDoc extends mongoose.Document {
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
+  findByEvent(event: { id: string; version: number }): Promise<TicketDoc | null>;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -48,6 +49,10 @@ ticketSchema.plugin(updateIfCurrentPlugin);
 
 // We need to force the mongo _id so we have stable ids across services
 ticketSchema.statics.build = ({ id, ...attrs }: TicketAttrs) => new Ticket({ _id: id, ...attrs });
+
+// We only want to process incremental updates to avoid concurrency issues, verify using version
+ticketSchema.statics.findByEvent = ({ id, version }: { id: string; version: number }) =>
+  Ticket.findOne({ _id: id, version: version - 1 });
 
 ticketSchema.methods.isReserved = async function () {
   // In this function, `this` references the current ticket

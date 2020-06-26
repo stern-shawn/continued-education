0c90d92ef90f643f57d7994ec9201993984cfdd2
Shawn Stern <stern.shawn@gmail.com>
2020-06-25 21:43:29 -0700

    feat: test the update blocking functionality

--- microservices/basic-microservices-arch/ticketing/tickets/src/routes/__test__/update.test.ts
+++ microservices/basic-microservices-arch/ticketing/tickets/src/routes/__test__/update.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import mongoose from 'mongoose';
 import { app } from '../../app';
 import { natsClient } from '../../nats-client';
+import { Ticket } from '../../models/ticket';
 
 it('returns a 404 if the provided id does not exist', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -110,4 +111,28 @@ it('publishes events', async () => {
     .expect(200);
 
   expect(natsClient.client.publish).toHaveBeenCalled();
+});
+
+it('rejects updates to reserved tickets', async () => {
+  const cookie = global.signin();
+
+  const response = await request(app).post('/api/tickets').set('Cookie', cookie).send({
+    title: 'asldkfj',
+    price: 20,
+  });
+
+  const ticket = await Ticket.findById(response.body.id);
+
+  ticket!.set({ orderId: mongoose.Types.ObjectId().toHexString() });
+  await ticket!.save();
+
+  // Edit should fail since the ticket is 'reserved' now
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'new title',
+      price: 100,
+    })
+    .expect(400);
 });

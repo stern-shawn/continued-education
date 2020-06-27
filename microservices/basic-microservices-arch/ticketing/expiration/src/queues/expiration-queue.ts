927a5fe39f90f422628761a92980bcc1217820a7
Shawn Stern <stern.shawn@gmail.com>
2020-06-27 12:43:51 -0700

    feat: basic impl of order created listener + bull queue for handling expiration jobs

---
+++ microservices/basic-microservices-arch/ticketing/expiration/src/queues/expiration-queue.ts
@@ -0,0 +1,17 @@
+import Queue from 'bull';
+
+interface ExpirationJobPayload {
+  orderId: string;
+}
+
+const expirationQueue = new Queue<ExpirationJobPayload>('order:expiration', {
+  redis: {
+    host: process.env.REDIS_HOST,
+  },
+});
+
+expirationQueue.process(async (job) => {
+  console.log(`TODO: publish an expiration:complete event for orderId: ${job.data.orderId}`);
+});
+
+export { expirationQueue };

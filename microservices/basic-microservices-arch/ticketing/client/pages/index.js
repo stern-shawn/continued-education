8e3d330f325bfdbf957691d4ff2fec0649fb9a5f
Shawn Stern <stern.shawn@gmail.com>
2020-06-29 19:50:20 -0700

    feat: add a basic form to create tickets + redirect to landing with a list of their tickets

--- microservices/basic-microservices-arch/ticketing/client/pages/index.js
+++ microservices/basic-microservices-arch/ticketing/client/pages/index.js
@@ -1,10 +1,32 @@
-const LandingPage = ({ currentUser }) => {
-  return <h1>{`You are ${currentUser == null ? 'NOT' : ''} signed in`}</h1>;
+const LandingPage = ({ currentUser, tickets }) => {
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tickets.map((ticket) => (
+            <tr key={ticket.id}>
+              <td>{ticket.title}</td>
+              <td>{ticket.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 // Component getInitialProps.context = { req, res }
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-  return {};
+  const { data: tickets } = await client.get('/api/tickets');
+
+  return { tickets };
 };
 
 export default LandingPage;
